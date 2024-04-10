070cc29c07559365aad99a3f595d9d237451ef5b
AnkitaDhek <122012285+AnkitaDhek@users.noreply.github.com>
2024-04-10 14:34:05 +0530

    //day 6

---
+++ import export/call.mjs
@@ -0,0 +1,11 @@
+const sum=(num1,num2)=>{
+    return (num1+num2);
+}
+const mul=(num1,num2)=>{
+return (num1*num2);
+}
+const print=(msg)=>{
+    console.log(msg);
+}
+export { sum as add ,mul as multiply };
+export default print;
---
+++ import export/main.mjs
@@ -0,0 +1,7 @@
+import {add,multiply} from"./call.mjs";
+import display from "./call.mjs"
+let s=add(10,20);
+let m=multiply(10,20);
+console.log("sum=",s);
+console.log("multiply=",m);
+display("this is print");
